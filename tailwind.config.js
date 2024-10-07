9e98a3944a350a842e298af83830e9f14ae00095
Jen Thomas James <jenthomasjames@gmail.com>
2024-10-07 19:45:35 +0530

    config: change inter to default sans font in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
     },
     fontFamily: {
-      inter: ["Inter", "sans-serif"],
+      sans: ["Inter", "sans-serif"],
       poppins: ["Poppins", "sans-serif"],
     },
   },
