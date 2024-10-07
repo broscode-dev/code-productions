87da920843c8b5f29dff249d75f2c1bf7f0b9baf
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2024-10-07 10:49:40 +0530

    feat: add fonts and brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#fff7ed",
+          100: "#ffecd4",
+          200: "#ffd5a8",
+          300: "#ffb771",
+          400: "#ff8d38",
+          500: "#fe721a",
+          600: "#ef5007",
+          700: "#c63a08",
+          800: "#9d300f",
+          900: "#7e2910",
+          950: "#441206",
+        },
+
+        secondary: {
+          50: "#06050B",
+        },
+      },
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
 };
