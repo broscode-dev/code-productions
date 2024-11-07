8cda7983946cbd3f4057eb239c598ec25c335ea6
Jen Thomas James <jenthomasjames@gmail.com>
2024-11-07 20:01:54 +0530

    style: fix button checked style

--- scripts/services.js
+++ scripts/services.js
@@ -1,10 +1,25 @@
-function showContent(serviceId) {
+function showContent(serviceEl) {
+  highlightRadioButton(serviceEl.id);
   const allContent = document.querySelectorAll(".service-content");
 
   allContent.forEach((div) => div.classList.add("hidden"));
 
-  if (serviceId) {
-    serviceId.classList.remove("hidden");
-    serviceId.classList.add("block");
+  if (serviceEl) {
+    serviceEl.classList.remove("hidden");
+    serviceEl.classList.add("block");
   }
 }
+
+const highlightRadioButton = (serviceId) => {
+  const radioButtonNumber = getNumberFromString(serviceId);
+  const radioButtonId = `service-radio-${radioButtonNumber}`;
+  const radioButton = document.getElementById(radioButtonId);
+  radioButton.checked = true;
+};
+
+const getNumberFromString = (string) => {
+  // Regex to find a number at the end of the string
+  const pattern = /(\d{1,2})$/;
+  const match = string.match(pattern);
+  return match ? match[0] : null;
+};
